82c75e175a00f8384dc7241a299481b94a52a495
msquaredds <alex.melesko@msquaredds.com>
2023-12-20 13:21:27 -0500

    Importing stauth

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import yaml
 from yaml.loader import SafeLoader
-import streamlit_authenticator as stauth
-#import importlib
-#stauth = importlib.import_module("streamlit-authenticator-test")
+#import streamlit_authenticator as stauth
+import importlib
+stauth = importlib.import_module("streamlit-authenticator-test")
 #stauth = __import__("streamlit-authenticator-test")
 
 
