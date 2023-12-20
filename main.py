dd9cac2523cbf0af402561366704287b81e14ade
msquaredds <alex.melesko@msquaredds.com>
2023-12-20 12:43:03 -0500

    Adding yaml file and pulling it in

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import streamlit as st
-
+import streamlit-authenticator-test as stauth
+import yaml
+from yaml.loader import SafeLoader
 
 def main():
     st.write('Hello World!')
+
+    with open('../config.yaml') as file:
+        config = yaml.load(file, Loader=SafeLoader)
+
+    st.write(config)
+
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],
+        config['preauthorized']
+    )
 
 
 if __name__ == '__main__':
