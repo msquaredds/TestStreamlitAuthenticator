9a7b16902b374af44854380e83549108e446efad
msquaredds <alex.melesko@msquaredds.com>
2023-12-20 14:39:20 -0500

    Updating the package

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ from yaml.loader import SafeLoader
 def main():
     st.write('Hello World!')
 
+    import sys
+    st.write(sys.version)
+
+    import pkg_resources
+    installed_packages = pkg_resources.working_set
+    installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+                                      for i in installed_packages])
+    st.write(installed_packages_list)
+
     # use for testing, but ideally we want to store and load from a more
     # secure location, like a database
     with open('config.yaml') as file:
