d0178a85ba1ffec9a2874ef7be139fbf02a9e3af
msquaredds <alex.melesko@msquaredds.com>
2023-12-28 11:56:11 -0500

    Trying login test

--- main.py
+++ main.py
@@ -46,13 +46,29 @@ def main():
     ##########################################################
     # Sign Up - Yes Preauthorization
     ##########################################################
-    try:
-        if authenticator.register_user('Register user'):
-            st.success('User registered successfully')
-    except Exception as e:
-        st.error(e)
+    # try:
+    #     if authenticator.register_user('Register user'):
+    #         st.success('User registered successfully')
+    # except Exception as e:
+    #     st.error(e)
+    #
+    # st.write(config)
 
-    st.write(config)
+    ##########################################################
+    # Login
+    ##########################################################
+    authenticator.login('Login', 'main')
+
+    if st.session_state["authentication_status"]:
+        authenticator.logout('Logout', 'main', key='unique_key')
+        st.write(f'Welcome *{st.session_state["name"]}*')
+        st.title('Some content')
+    elif st.session_state["authentication_status"] is False:
+        st.error('Username/password is incorrect')
+    elif st.session_state["authentication_status"] is None:
+        st.warning('Please enter your username and password')
+
+
 
 
 if __name__ == '__main__':
