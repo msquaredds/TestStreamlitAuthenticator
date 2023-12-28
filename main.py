c63843aec52f529592007b9ad2f7bbe94c8586f7
msquaredds <alex.melesko@msquaredds.com>
2023-12-28 12:24:16 -0500

    Adding forgot password

--- main.py
+++ main.py
@@ -57,16 +57,16 @@ def main():
     ##########################################################
     # Login / Logout
     ##########################################################
-    authenticator.login('Login', 'main')
-
-    if st.session_state["authentication_status"]:
-        authenticator.logout('Logout', 'main', key='unique_key')
-        st.write(f'Welcome *{st.session_state["name"]}*')
-        st.title('Some content')
-    elif st.session_state["authentication_status"] is False:
-        st.error('Username/password is incorrect')
-    elif st.session_state["authentication_status"] is None:
-        st.warning('Please enter your username and password')
+    # authenticator.login('Login', 'main')
+    #
+    # if st.session_state["authentication_status"]:
+    #     authenticator.logout('Logout', 'main', key='unique_key')
+    #     st.write(f'Welcome *{st.session_state["name"]}*')
+    #     st.title('Some content')
+    # elif st.session_state["authentication_status"] is False:
+    #     st.error('Username/password is incorrect')
+    # elif st.session_state["authentication_status"] is None:
+    #     st.warning('Please enter your username and password')
 
     ##########################################################
     # Forgot Username
@@ -85,6 +85,18 @@ def main():
     ##########################################################
     # Forgot Password
     ##########################################################
+    try:
+        (username_of_forgotten_password, email_of_forgotten_password,
+         new_random_password) = authenticator.forgot_password('Forgot password')
+        if username_of_forgotten_password:
+            st.success('New password to be sent securely')
+            st.write(new_random_password)
+            # Random password should be transferred to user securely
+        else:
+            st.error('Username not found')
+    except Exception as e:
+        st.error(e)
+
 
 
 if __name__ == '__main__':
