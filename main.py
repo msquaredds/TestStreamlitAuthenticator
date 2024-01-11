251b4d3e15dcca83f78434c524f57ffeff557600
msquaredds <alex.melesko@msquaredds.com>
2024-01-11 14:13:35 -0500

    Testing register_user

--- main.py
+++ main.py
@@ -50,11 +50,13 @@ def main():
     if ('stauth' in st.session_state and
             'dev_errors' in st.session_state['stauth'].keys() and
             'register_user' in st.session_state['stauth']['dev_errors'].keys()):
-        st.error(st.session_state['stauth']['dev_errors']['register_user'])
+        st.error(f"dev_error: "
+                 f"{st.session_state['stauth']['dev_errors']['register_user']}")
     elif ('stauth' in st.session_state and
           'user_errors' in st.session_state['stauth'].keys() and
           'register_user' in st.session_state['stauth']['user_errors'].keys()):
-        st.error(st.session_state['stauth']['user_errors']['register_user'])
+        st.error(f"user_error: "
+                 f"{st.session_state['stauth']['user_errors']['register_user']}")
 
 
 
