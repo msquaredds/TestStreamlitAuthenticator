a0a9fa446d5ce510e8f9b2649ef28fa5ac65dbb1
msquaredds <alex.melesko@msquaredds.com>
2024-01-12 12:48:41 -0500

    Testing register_user

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
     ##########################################################
     stauth.ErrorHandling.clear_errors()
 
-    authenticator.register_user('main', False, 'generic')
+    authenticator.register_user('main', True, 'generic')
 
     if ('stauth' in st.session_state and
             'dev_errors' in st.session_state['stauth'].keys() and
