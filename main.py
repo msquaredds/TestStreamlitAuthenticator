f4f31380319946879751d0770dd07bbae60b4ac9
msquaredds <alex.melesko@msquaredds.com>
2024-06-13 17:45:59 -0400

    Starting to test the new login function

--- main.py
+++ main.py
@@ -251,6 +251,8 @@ def main():
             st.error(f"user_error: "
                      f"{st.session_state['stauth']['user_errors']['login']}")
 
+        st.write("TEST")
+
         authenticator.login(location='main',
                             password_pull_function='bigquery',
                             password_pull_args={
