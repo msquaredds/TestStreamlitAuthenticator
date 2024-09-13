20f8cd7d04e7c9b631547b89657296832c107a24
msquaredds <alex.melesko@msquaredds.com>
2024-09-13 16:06:21 -0400

    Testing update_user_info

--- main.py
+++ main.py
@@ -400,7 +400,7 @@ def main():
                      f"{st.session_state['stauth']['user_errors']['update_user_info']}")
 
         authenticator.update_user_info(
-            location='hi',
+            location='main',
             info_pull_function='bigquery',
             info_pull_args={
                 'bq_creds': st.secrets['BIGQUERY'],
