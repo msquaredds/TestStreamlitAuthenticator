c8eeba76fb3fdc91191ee1b84d638a90ab2c18c1
msquaredds <alex.melesko@msquaredds.com>
2024-09-30 14:20:10 -0400

    Testing new login

--- main.py
+++ main.py
@@ -70,12 +70,12 @@ def main():
                 'website_name': 'SharpShares',
                 'website_email': 'hello@sharpshares.com'},
             email_creds={'sendgrid_api_key':
-                             st.secrets['SENDGRID']['sendgrid_api_key']},
-            save_pull_function='bigquery',
-            save_pull_args={
-                'bq_creds': st.secrets['BIGQUERY'],
-                'project': 'teststreamlitauth-412915',
-                'dataset': 'test_credentials'})
+                             st.secrets['SENDGRID']['sendgrid_api_key']},)
+            # save_pull_function='bigquery',
+            # save_pull_args={
+            #     'bq_creds': st.secrets['BIGQUERY'],
+            #     'project': 'teststreamlitauth-412915',
+            #     'dataset': 'test_credentials'})
     except ValueError as e:
         # there are only dev errors for class instantiation and they
         # wouldn't need to show up ahead of time, just if they occur
@@ -92,9 +92,9 @@ def main():
     sterr.display_error('user_errors', 'register_user')
 
     # most of the arguments were already passed to the class instantiation
-    authenticator.register_user(
-        'main',
-        cred_save_args={'table_name': 'user_credentials'})
+    # authenticator.register_user(
+    #     'main',
+    #     cred_save_args={'table_name': 'user_credentials'})
 
     sterr.display_error('dev_errors', 'register_user', False)
     sterr.display_error('user_errors', 'register_user', False)
@@ -135,8 +135,11 @@ def main():
             'datetime_col': 'datetime'}
 
         authenticator.login(location='main',
-                            # password_pull_function='bigquery',
+                            password_pull_function='bigquery',
                             password_pull_args={
+                                'bq_creds': st.secrets['BIGQUERY'],
+                                'project': 'teststreamlitauth-412915',
+                                'dataset': 'test_credentials',
                                 'table_name': 'user_credentials',
                                 'username_col': 'username',
                                 'password_col': 'password'},
