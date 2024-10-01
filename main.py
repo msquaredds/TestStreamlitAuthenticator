8762337d94b027430e0b5e9f0ab148d089c4bae1
msquaredds <alex.melesko@msquaredds.com>
2024-10-01 08:42:08 -0400

    Retesting register_user 9

--- main.py
+++ main.py
@@ -94,14 +94,14 @@ def main():
     # most of the arguments were already passed to the class instantiation
     authenticator.register_user(
         'main',
-        cred_save_function='bigquery',
+        # cred_save_function='bigquery',
         cred_save_args={'table_name': 'user_credentials',
                         'bq_creds': st.secrets['BIGQUERY'],
                         # 'project': 'teststreamlitauth-412915',
                         'dataset': 'test_credentials',
                         })
 
-    st.write("test8")
+    st.write("test9")
 
     sterr.display_error('dev_errors', 'register_user', False)
     sterr.display_error('user_errors', 'register_user', False)
