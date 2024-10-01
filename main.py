37bdd39df1582ab2896e7ebe2a69694cbba0faf1
msquaredds <alex.melesko@msquaredds.com>
2024-10-01 08:04:56 -0400

    Retesting register_user 2

--- main.py
+++ main.py
@@ -95,12 +95,12 @@ def main():
     authenticator.register_user(
         'main',
         cred_save_args={'table_name': 'user_credentials',
-                        'bq_creds': st.secrets['BIGQUERY'],
+                        # 'bq_creds': st.secrets['BIGQUERY'],
                         'project': 'teststreamlitauth-412915',
                         'dataset': 'test_credentials'
                         })
 
-    st.write("test1")
+    st.write("test2")
 
     sterr.display_error('dev_errors', 'register_user', False)
     sterr.display_error('user_errors', 'register_user', False)
