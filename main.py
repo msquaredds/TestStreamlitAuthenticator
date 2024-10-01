0ce9c73a42373ca547e26c84112cf800b13486ea
msquaredds <alex.melesko@msquaredds.com>
2024-10-01 08:38:24 -0400

    Retesting register_user 5

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def main():
     authenticator.register_user(
         'main',
         # cred_save_function='bigquery',
-        cred_save_args={'table_name': 'user_credentials',})
+        cred_save_args={'table_name': 'user_credentials', "hi": "hi"})
                         # 'bq_creds': st.secrets['BIGQUERY'],
                         # 'project': 'teststreamlitauth-412915',
                         # 'dataset': 'test_credentials',
