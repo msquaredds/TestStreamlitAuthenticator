2eea1fd3104aeec03eb6b1eca86b1c300b4a3f2f
msquaredds <alex.melesko@msquaredds.com>
2024-10-01 07:53:22 -0400

    Testing new login

--- main.py
+++ main.py
@@ -70,12 +70,12 @@ def main():
                 'website_name': 'SharpShares',
                 'website_email': 'hello@sharpshares.com'},
             email_creds={'sendgrid_api_key':
-                             st.secrets['SENDGRID']['sendgrid_api_key']},)
-            # save_pull_function='bigquery',
-            # save_pull_args={
-            #     'bq_creds': st.secrets['BIGQUERY'],
-            #     'project': 'teststreamlitauth-412915',
-            #     'dataset': 'test_credentials'})
+                             st.secrets['SENDGRID']['sendgrid_api_key']},
+            save_pull_function='bigquery',
+            save_pull_args={
+                'bq_creds': st.secrets['BIGQUERY'],
+                'project': 'teststreamlitauth-412915',
+                'dataset': 'test_credentials'})
     except ValueError as e:
         # there are only dev errors for class instantiation and they
         # wouldn't need to show up ahead of time, just if they occur
