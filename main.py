a66b9060656abdd45bc9ce7f74bc01be06c61fb9
msquaredds <alex.melesko@msquaredds.com>
2024-11-13 15:33:48 -0500

    Setting up nagivation and pages 2

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def main():
                        f'font-weight: bold;">{title_writing}</p>'
         st.markdown(title_format, unsafe_allow_html=True)
 
-    pages = [st.Page("/NavPages/Authentication.py", title="Authentication"),
-             st.Page("/NavPages/EmailVerification.py",
+    pages = [st.Page("../NavPages/Authentication.py", title="Authentication"),
+             st.Page("../NavPages/EmailVerification.py",
                      title="Email Verification")]
     pg = st.navigation(pages)
     pg.run()
